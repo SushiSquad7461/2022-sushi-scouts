79779ac48979f3bf6b35be86ed182506635081eb
Daniel Gorbatov <dg20@uw.edu>
2022-01-18 18:59:29 -0800

    Basic setup

---
+++ pages/scouting-config.js
@@ -0,0 +1,28 @@
+/*
+    Format for adding data point:
+
+    "name" : {
+        "type": "number",
+        "label": "Team Number",
+        "default": "12" (null for no default),
+        "values" (if applicable, if not leave as empty array): ["low climb", "no climb", "high climb"]
+    }
+
+*/
+
+const data = {
+    "MatchData": [
+
+    ],
+    "Auto": [
+
+    ],
+    "TeleOp": [
+
+    ],
+    "Endgame": [
+
+    ]
+};
+
+module.exports = data;
