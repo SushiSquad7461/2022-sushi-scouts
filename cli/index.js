2b802871d409d49016bb7a827c4538d93224ae13
Daniel Gorbatov <59454383+dgorbatov@users.noreply.github.com>
2022-02-07 22:11:51 +0000

    Created team profile in script

--- cli/index.js
+++ cli/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const config = require("../data/teamconfig.json");
 
+const FRCDistrictCodes = ["CHS", "FIM", "TX", "IN", "IRS", "FMA", "FNC", "NE", "ONT", "PNW", "PHC"]
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 function execShellCommand(cmd) {
@@ -22,31 +23,60 @@ function execShellCommand(cmd) {
   });
 }
 
+async function collectTeamData() {
+  const answers = await inquirer.prompt([{
+    name: 'teamNumber',
+    type: 'input',
+    message: 'What is your team number?',
+    validate: input => {
+      return !isNaN(parseInt(input)) ? true : "Invalid input";
+    },
+    filter: input => { return !isNaN(parseInt(input)) ?parseInt(input) : input; }
+  }, {
+   name: "districtCode",
+  message: "What is your teams district code?",
+   type: "list",
+   choices: FRCDistrictCodes
+  }]);
+
+  answers.gotData = true;
+
+  fs.writeFile("./data/teamconfig.json", JSON.stringify(answers), () => {});
+}
+
 (async () => {
   console.clear();
 
   console.log("Welcome to Sushi Scouts....");
-  
+
   await sleep(1000);
   
+  console.log();
+
+
   if (!config.gotData) {
-    const answers = await inquirer.prompt({
-      name: 'player_name',
-      type: 'input',
-      message: 'What is your name?',
-      default() {
-          return 'Player';
-      },
-      });
-  
-      console.log(answers);
-  
-      config.gotData = true;
+    await collectTeamData();
   } else {
-    console.log("Welcome back");
+    console.log("Welcome back Team " + config.teamNumber);
+  }
+
+  console.log("\n\n");
+
+  let input = "";
+  while (input !== "Quit") {
+    const answers = await inquirer.prompt({
+      name: "choice",
+      message: "Enter a command you want to run: ",
+      type: "list",
+      choices: ["Run App", "Quit"]
+    }).catch(err => console.log(err));
+
+    input = answers.choice;
+
+    if (intput === "Run App") {
+      console.log("Satarting up Sushi Scouts....")
+      execShellCommand("npm run dev");
+    }
   }
   
-  fs.writeFile("./data/teamconfig.json", JSON.stringify(config), () => {});
-  
-  execShellCommand("npm run dev");
 })();
