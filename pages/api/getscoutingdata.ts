437a1356348b72196df57753e3eb34f923ce988d
Daniel Gorbatov <dg20@uw.edu>
2022-03-25 21:47:28 -0700

    prisma doneish

--- pages/api/getscoutingdata.ts
+++ pages/api/getscoutingdata.ts
@@ -1,13 +1,11 @@
 import type {NextApiRequest, NextApiResponse} from "next";
-import {readFileSync} from "fs";
 import {stringify} from "csv-stringify/sync";
-const MATCH_JSON_FILE = "./data/matchdata.json";
 import stream from "stream";
 import {promisify} from "util";
 import {PrismaClient} from "@prisma/client";
 
 const pipeline = promisify(stream.pipeline);
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 /**
  * Assigns team number for computer to scout based on match schedule
@@ -19,11 +17,6 @@ export default async function handler(
     res: NextApiResponse,
 ) {
   const matchData = await prisma.matchdata.findMany();
-  // const jsonFile = readFileSync(MATCH_JSON_FILE);
-
-  // // This is a bit dangerous as it makes
-  // // assumptions about the format of the JSON file.
-  // const {matchData} = JSON.parse(jsonFile.toString());
 
   const headers = Object.keys(matchData[0]);
   const allValues = matchData.map((match: { [s: string]: unknown; } |
@@ -37,7 +30,4 @@ export default async function handler(
   res.setHeader("Content-Type", "application/text");
   res.setHeader("Content-Disposition", "attachment; filename=scoutingdata.csv");
   await pipeline(csvOutput, res);
-
-  // Send scouting data
-  // res.status(200).json({"data": csvOutput});
 }
--- pages/api/submiteventinfo.ts
+++ pages/api/submiteventinfo.ts
@@ -1,9 +1,8 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 // import users from "../../data/matchdata.json";
-import {readFileSync, writeFile, writeFileSync} from "fs";
+import {readFileSync, writeFileSync} from "fs";
 import {MatchSchedule} from "../../data/scouting-config";
-import {PrismaClient} from '@prisma/client'
-import { match } from "assert";
+import {PrismaClient} from "@prisma/client";
 
 const filePath = "./data/matchschedule.json";
 const prisma = new PrismaClient();
@@ -38,34 +37,27 @@ export default async function handler(
 
   await prisma.matchdata.create({
     data: {
-      'teamNumber' : matchData["teamNumber"],
-      'comp' : matchData['comp'],
-      'matchNum' : parseInt(matchData['match info:match #']),
-      'matchType' : matchData['match info:match type'],
-      'teamScouted' : matchData["match info:team # you're scouting"],
-      'stationId' : matchData["match info:station id"],
-      'auto_show' : matchData["auto:no show"] !== "off",
-      'auto_move' : matchData["auto:move off tarmac"] !== "off",
-      "auto_lowHub" : matchData["auto:scored lower hub"],
-      "auto_humanPlayer" : matchData["auto:scored by hp"],
-      "auto_upperHub" : matchData["auto:scored upper hub"],
-      "teleop_groundPickup" : matchData["teleop:ground pickup"] !== "off",
-      "teleop_terminalPickup" : matchData["teleop:terminal pickup"] !== "off",
-      "teleop_lowHub" : matchData["teleop:scored lower hub"],
-      "teleop_highHub" : matchData["teleop:scored upper hub"],
-      "end_climb" : matchData["end game:climb"],
-      "end_climbLevel" : matchData["end game:climb type"],
-      "end_defense" : matchData["end game:defense"] !== "off",
-      "end_notes" : matchData["end game:notes"],
-    }
+      "teamNumber": matchData["teamNumber"],
+      "comp": matchData["comp"],
+      "matchNum": parseInt(matchData["match info:match #"]),
+      "matchType": matchData["match info:match type"],
+      "teamScouted": parseInt(matchData["match info:team # you're scouting"]),
+      "stationId": matchData["match info:station id"],
+      "auto_show": matchData["auto:no show"],
+      "auto_move": matchData["auto:move off tarmac"],
+      "auto_lowHub": matchData["auto:scored lower hub"],
+      "auto_humanPlayer": matchData["auto:scored by hp"],
+      "auto_upperHub": matchData["auto:scored upper hub"],
+      "teleop_groundPickup": matchData["teleop:ground pickup"],
+      "teleop_terminalPickup": matchData["teleop:terminal pickup"],
+      "teleop_lowHub": matchData["teleop:scored lower hub"],
+      "teleop_highHub": matchData["teleop:scored upper hub"],
+      "end_climb": matchData["end game:climb"],
+      "end_climbLevel": matchData["end game:climb type"],
+      "end_defense": matchData["end game:defense"],
+      "end_notes": matchData["end game:notes"],
+    },
   });
-
-  // writeFile("./data/matchdata.json",
-  //     JSON.stringify(users),
-  //     (err: Error | null) => {
-  //       // If error return 500 message
-  //       if (err) res.status(500).json({result: err.message});
-  //     });
 
   // Successfully added data
   res.status(200).json({result: "success"});
