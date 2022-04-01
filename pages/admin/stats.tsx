c01be9830f2522499b82a821d929ca94638fff10
Daniel Gorbatov <dg20@uw.edu>
2022-04-01 08:25:51 -0700

    update stats

--- pages/admin/stats.tsx
+++ pages/admin/stats.tsx
@@ -80,7 +80,11 @@ const Stats: NextPage = () => {
               <article key={element.stationId}>
                 <svg className={statstyles.laptop} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M20 18c1.1 0 1.99-.9 1.99-2L22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H1c-.55 0-1 .45-1 1s.45 1 1 1h22c.55 0 1-.45 1-1s-.45-1-1-1h-3zM5 6h14c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1H5c-.55 0-1-.45-1-1V7c0-.55.45-1 1-1z"/>
                 </svg>
-                <p>{element.stationId}</p>
+                <p className={element.submitted ?
+                  statstyles.submitted:
+                  statstyles.notsubmitted}>
+                  {element.stationId}
+                </p>
                 {element.scouting ?
                   <svg className={statstyles.status} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 64 64"><circle cx="32" cy="32" r="30" fill="#4bd37b"/><path fill="#fff" d="M46 14L25 35.6l-7-7.2l-7 7.2L25 50l28-28.8z"/></svg> :
                   <svg className={statstyles.status} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path fill="red" d="M10 1c-5 0-9 4-9 9s4 9 9 9s9-4 9-9s-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7s7 3.1 7 7s-3.1 7-7 7zM6 9v2h8V9H6z" className="st0"/></svg>
--- pages/api/getstats.ts
+++ pages/api/getstats.ts
@@ -1,11 +1,10 @@
 import type {NextApiRequest, NextApiResponse} from "next";
-import {readFileSync} from "fs";
-import {MatchSchedule} from "../../data/scouting-config";
-const filePath = "./data/matchschedule.json";
+import {prisma} from "../../lib/prisma";
 
 export type currScoutingType = {
         stationId: string,
-        scouting: boolean
+        scouting: boolean,
+        submitted: boolean
 }
 
 export type statsType = {
@@ -22,65 +21,57 @@ type Data = {
  * @param {NextApiRequest} req Api request object
  * @param {NextApiResponse} res Api response object
  */
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 ) {
-  const schedule: MatchSchedule = JSON.parse(readFileSync(filePath).toString());
-  let currQual: number = parseInt(req.query["currQual"].toString());
-  let maxQual: number = parseInt(req.query["maxQual"].toString());
-  const lastMatch: number = schedule["matches"].length;
+  const currQual: number = parseInt(req.query["currQual"].toString());
+  const maxQual: number = parseInt(req.query["maxQual"].toString());
 
   if (isNaN(currQual) || isNaN(maxQual)) {
     res.status(400).json({"currScouting": [], "stats": []});
   } else {
-    if (currQual > lastMatch) {
-      currQual = lastMatch;
-    }
-
-    if (maxQual > lastMatch) {
-      maxQual = lastMatch;
-    }
-
     const currScouting: Array<currScoutingType> = [];
     const stats: Array<statsType> = [];
 
     // Get current scouting rate CSR, and ppl currently scouting
-    let submitted = 0;
-    for (const i of Object.keys(schedule["matches"][currQual-1])) {
-      currScouting.push({stationId: i, scouting:
-            (schedule["matches"][currQual-1][i].numScouting > 0)});
-
-      if (schedule["matches"][currQual-1][i].submitted) {
-        submitted += 1;
-      }
-    }
-
-    stats.push({"CSR": Math.round(((submitted / 6) * 100)) + "%"});
+    const submittedCurrQual = await prisma.schedule.findMany({
+      where: {
+        matchNum: currQual,
+        completed: true,
+      },
+    });
+    stats.push({"CSR": Math.round((submittedCurrQual.length / 6) * 100) + "%"});
 
     // Calculate OSR, overall submission rate
-    submitted = 0;
-    for (let i=0; i < maxQual; ++i) {
-      for (const j of Object.keys(schedule["matches"][i])) {
-        if (schedule["matches"][i][j].submitted) {
-          submitted += 1;
-        }
-      }
-    }
+    const submittedOverall = await prisma.schedule.findMany({
+      where: {
+        matchNum: {
+          lte: maxQual,
+        },
+        completed: true,
+      },
+    });
 
-    stats.push({"OSR": Math.round(((submitted / (maxQual*6)) * 100)) + "%"});
+    stats.push({"OSR": Math.round(((submittedOverall.length /
+      (maxQual*6)) * 100)) + "%"});
 
     // Calculate LSS and MSS, least submitted station, most submitted station
     const stationCounts: {[index: string]: number} = {"R1": 0,
       "R2": 0, "R3": 0, "B1": 0, "B2": 0, "B3": 0};
-    for (let i=0; i < maxQual; ++i) {
-      for (const j of Object.keys(schedule["matches"][i])) {
-        if (schedule["matches"][i][j].submitted) {
-          stationCounts[j] += 1;
-        }
-      }
-    }
 
+    const allSubmitedEntries = await prisma.schedule.findMany({
+      where: {
+        matchNum: {
+          lte: maxQual,
+        },
+        completed: true,
+      },
+    });
+
+    for (let i=0; i < allSubmitedEntries.length; ++i) {
+      stationCounts[allSubmitedEntries[i].stationId] += 1;
+    }
 
     let min = "R1";
     let max = "R1";
@@ -94,6 +85,24 @@ export default function handler(
 
     stats.push({"LSS": min});
     stats.push({"MSS": max});
+    stats.push({"R1": stationCounts.R1.toString()});
+    stats.push({"R2": stationCounts.R2.toString()});
+    stats.push({"R3": stationCounts.R3.toString()});
+    stats.push({"B1": stationCounts.B1.toString()});
+    stats.push({"B2": stationCounts.B2.toString()});
+    stats.push({"B3": stationCounts.B3.toString()});
+
+    // Get curr scouting
+    const currMatches = await prisma.schedule.findMany({
+      where: {
+        matchNum: currQual,
+      },
+    });
+
+    for (const i of currMatches) {
+      currScouting.push({stationId: i.stationId,
+        scouting: i.numScouting > 1, submitted: i.completed});
+    }
 
     res.status(200).json({"currScouting": currScouting, "stats": stats});
   }
